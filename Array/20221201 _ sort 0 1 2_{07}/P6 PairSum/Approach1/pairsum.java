9da6c82fd051b9427c4e6aef0d321ef4e1594279
Hanumanth <48180907+HANAMANTAPPA@users.noreply.github.com>
2022-12-03 00:55:56 +0530

    Update the code

--- Array/20221201 _ sort 0 1 2_{07}/P6 PairSum/Approach1/pairsum.java
+++ Array/20221201 _ sort 0 1 2_{07}/P6 PairSum/Approach1/pairsum.java
@@ -1,3 +1,47 @@
 
 
+import java.util.* ;
+import java.io.*; 
+import java.util.* ;
+import java.io.*; 
+public class Solution{
+    public static int[][] pairSum(int[] arr, int s) {
+        // Write your code here.
+        /// sort the given array 
+        for(int i=0; i< arr.length-1; i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]> arr[j+1]){
+                    int temp=arr[i];
+                    arr[i] =arr[j];
+                    arr[j]= temp;
+                }
+            }
+        }
+        
+        ArrayList<Integer> al=new ArrayList<integer>();
+        
+        for (int i=0; i<arr.length-1; i++){ //let's take till 2nd last element 
+            for(int j=i+1; j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    al.add(arr[i]);
+                    al.add(arr[j]);
+                }
+            }
+        }
+        // add arr list to  an array 
+        int sizeofarr= al.size()/2;
+        int[][] oarr=new int[sizeofarr][2];
+        
+        int indexofAL=0;
+        
+        for (int i=0; i<sizeofarr ;i++){
+            for(int j=0; j<2; j++){
+                oarr[i][j] =al.get(indexofAL);
+                indexofAL++;
+            }
+        }
+        
+        return oarr;
+    }
+}
 
