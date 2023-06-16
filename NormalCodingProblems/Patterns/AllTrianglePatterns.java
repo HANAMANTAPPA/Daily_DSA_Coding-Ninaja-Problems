ff342567b06d10f955fd89ac1299b643495d1815
Hanumanth <48180907+HANAMANTAPPA@users.noreply.github.com>
2023-06-16 12:06:00 -0700

    Update AllTrianglePatterns.java

--- NormalCodingProblems/Patterns/AllTrianglePatterns.java
+++ NormalCodingProblems/Patterns/AllTrianglePatterns.java
@@ -54,6 +54,68 @@ public class MyClass {
 }
 
 /////--------------- Problem 03 ---------------------------------------------
+/*
+    1
+   121
+  12321
+ 1234321
+123454321
 
+*/
 
+public class MyClass {
+    public static void main(String args[]) {
+      int n=5;
+      int temp=n*(n+1)/2 ;
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            
+            int temp1=1;
+            for(int j=1; j<=i; j++){
+                System.out.print( temp1); //  System.out.print("*") for only stars
+                temp1++;
+            }
+            temp1=temp1-1;
+            for(int j=1; j<i ; j++){
+                System.out.print(--temp1);
+            }
+            System.out.println();
+        }
+    }
+}
+////---------------- Problem -------------
+/*
+1=1
+1+2=3
+1+2+3=6
+1+2+3+4=10
+1+2+3+4+5=15
+1+2+3+4+5+6=21
+
+*/
+
+public class MyClass {
+	
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            int sum=0;
+            for(int j=1;j<=i;j++){
+                sum+=j;
+                if(j==i){
+                    System.out.println(j+"="+sum);
+                }
+                else{
+                    System.out.print(j+"+");
+                }
+            }      
+        }
+    
+	}
+}
 
