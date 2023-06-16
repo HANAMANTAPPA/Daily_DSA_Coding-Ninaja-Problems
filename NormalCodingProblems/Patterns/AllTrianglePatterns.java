8d78d0a823870d062e37df4052726e8f4fb39a59
Hanumanth <48180907+HANAMANTAPPA@users.noreply.github.com>
2023-06-16 07:53:05 -0700

    Update AllTrianglePatterns.java

--- NormalCodingProblems/Patterns/AllTrianglePatterns.java
+++ NormalCodingProblems/Patterns/AllTrianglePatterns.java
@@ -1,10 +1,10 @@
-/*
-output 
-ABCDE
- ABCD
-  ABC
-   AB
-    A
+/* -------Problem 01 ----
+------ output 
+*****    OR  EDCBA     OR ABCDE
+ ****         DCBA         ABCD
+  ***          CBA          ABC
+   **           BA           AB
+    *            A            A
 */
 
 public class MyClass {
@@ -15,16 +15,45 @@ public class MyClass {
             for(int j=1; j<i; j++){
                 System.out.print(" ");
             }
-            int temp=0;
+            temp=n-i; // OR int temp=0;
             for(int j=1; j<=n-i+1; j++){
-                System.out.print( (char)('A' + (temp))); //  System.out.print("*") for only stars
-                temp++;
+                 ystem.out.print("*"); // or System.out.print( (char)('A' + (temp))); System.out.print( (char)('A' + (temp))); or 
+                temp--;// or temp++; 
             }
             System.out.println();
         }
     }
 }
-//////--------------------------------------------------------------------------
+//////--------------------------------------Problem 02------------------------------------
+/*  
+
+out put 
+ONMLK
+ JIHG
+  FED
+   CB
+    A
+*/
+
+public class MyClass {
+    public static void main(String args[]) {
+      int n=5;
+      int temp=n*(n+1)/2 ;
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<i; j++){
+                System.out.print(" ");
+            }
+            
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print( (char)('A' + (temp-1))); //  System.out.print("*") for only stars
+                temp--;
+            }
+            System.out.println();
+        }//////----------------------------------------
+    }
+}
+
+/////--------------- Problem 03 ---------------------------------------------
 
 
 
